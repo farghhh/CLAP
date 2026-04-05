b7ffd7bb184c06fc5ea4b0e9ac40785852a3422c
farghhh <nurinfarzana28@email.com>
2026-04-05 18:32:28 +0800

    fixed email host password

--- CLAP_project/settings.py
+++ CLAP_project/settings.py
@@ -171,7 +171,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 FRONTEND_URL = os.environ.get('FRONTEND_URL', 'https://farghhh.github.io/CLAP')
 
 # Whitenoise for static files
