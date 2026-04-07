cb2ef0988b7cddbcc01914eef89d48d373e5b9f1
aisyahibrhm <siti371910@gmail.com>
2026-04-07 16:14:24 +0800

    Fix rescheduled_count assignment in response data

--- schedule/views.py
+++ schedule/views.py
@@ -609,7 +609,7 @@ def check_missed(request):
 
     response_data = {
         'message': f"{missed_result['count']} missed session(s) rescheduled.",
-        'rescheduled_count': missed_result['count'],
+        'rescheduled_count': missed_result['missed_count'],
         'missed_count": missed_result.get('missed_count', missed_result['count']),
     }
 
